74bd0418f330cc16482bb9eaa2a66045c7815f1b
oleg <olegsapozhnik6699@gmail.com>
2023-12-19 16:04:38 +0200

    Add Android platform

--- Flappy-bird/Assets/Scripts/Player.cs
+++ Flappy-bird/Assets/Scripts/Player.cs
@@ -15,17 +15,31 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+            // Проверяем нажатие клавиши пробела
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
 
-        if (transform.position.y > ceilingHeight)
-        {
-            transform.position = new Vector2(transform.position.x, ceilingHeight);
-            rb.velocity = Vector2.zero;
+            // Проверяем сенсорный ввод
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.touches[0];
+
+                // Проверяем, что это было начало касания
+                if (touch.phase == TouchPhase.Began)
+                {
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                }
+            }
+
+            // Проверяем, если объект поднимается выше заданной высоты
+            if (transform.position.y > ceilingHeight)
+            {
+                transform.position = new Vector2(transform.position.x, ceilingHeight);
+                rb.velocity = Vector2.zero;
+            }
         }
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
